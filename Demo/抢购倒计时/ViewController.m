18a418c9fee9cbb6bfd1f7e1cde98621dc493849
ruixiaoguo <grx0917@sina.com>
2018-10-25 16:24:35 +0800

    Initial commit

---
+++ Demo/抢购倒计时/ViewController.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  抢购倒计时
+//
+//  Created by LGQ on 16/4/11.
+//  Copyright © 2016年 LGQ. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GGClockView.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) GGClockView *clockView;
+@property (nonatomic, assign) NSInteger index;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.index = 0;
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction)]];
+    
+    // 创建倒计时器
+    self.clockView = [GGClockView clockViewWithTimeBackgroundColor:[UIColor whiteColor]
+                                                     timeTextColor:[UIColor blackColor]
+                                                        colonColor:nil
+                                                              font:[UIFont boldSystemFontOfSize:14]];
+    self.clockView.contentMode = GGClockViewContentModeCenter;
+    self.clockView.isDisplayDay = NO;
+//    self.clockView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.clockView];
+    
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    self.clockView.frame = CGRectMake(100, 100, 150, 25);
+}
+
+#pragma mark - 控制方法
+- (void)tapAction {
+    NSInteger action = self.index % 4;
+    switch (action) {
+        case 0: // 开始计时
+            // 赋值，单位秒
+            self.clockView.time = 99999;
+            [self.clockView start];
+            break;
+        case 1: // 暂停
+            [self.clockView pause];
+            break;
+        case 2:
+            [self.clockView start];
+            break;
+        case 3: // 停止计时，计时器归零
+            [self.clockView stop];
+            break;
+    }
+    self.index ++;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
+
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# GGClockView
+A countdown view, similar to taobao limited-time sales  
+一个倒计时视图，类似于淘宝的限时抢购
+
+
+![image](https://github.com/251143492/GGClockView/blob/master/GGClockView.png)
+
+---
+可以自定义外观  
+
+* 文本字号      `font`
+* 文本颜色      `timeTextColor`
+* 文本框背景颜色 `timeBackgroundColor`
+* 冒号颜色      `colonColor`
+
+
+控制方法  
+
+* 计时开始   `- (void)start`
+* 暂停      `- (void)pause`
+* 停止      `- (void)stop`
